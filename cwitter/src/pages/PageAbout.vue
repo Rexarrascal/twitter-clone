b544159a25e8bddd895305b3116feade65005c55
Rexarrascal <serkanozytu@gmail.com>
2022-08-31 08:41:52 +0300

    Add home page and about page

---
+++ cwitter/src/pages/PageAbout.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h4 class="q-mt-none q-mb-md text-weight-bold">About Cwitter</h4>
+    <div class="text-body1">
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores amet porro nemo obcaecati cupiditate eos at nobis numquam voluptatum debitis ad, quaerat fugiat provident earum officiis rerum animi, necessitatibus enim!</p>
+      
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores amet porro nemo obcaecati cupiditate eos at nobis numquam voluptatum debitis ad, quaerat fugiat provident earum officiis rerum animi, necessitatibus enim!</p>
+    </div>
+    
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageAbout'
+})
+</script>
---
+++ cwitter/src/pages/PageHome.vue
@@ -0,0 +1,58 @@
+<template>
+  <q-page>
+    <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
+      <div class="col">
+        <q-input
+          class="new-Cweet"
+          bottom-slots
+          v-model="newCweetContent"
+          placeholder="What's happening?"
+          counter
+          maxlength="280"
+          autogrow
+        >
+          <template v-slot:before>
+            <q-avatar size="xl">
+              <img src="https://cdn.quasar.dev/img/avatar5.jpg" />
+            </q-avatar>
+          </template>
+        </q-input>
+      </div>
+      
+      <div class="col col-shrink">
+          <q-btn 
+            :disable="!newCweetContent"
+            class="q-mb-lg"
+            uneleavated
+            rounded
+            color="primary" 
+            label="Cweet"
+            no-caps
+          />
+      </div>
+    </div>
+
+    <q-separator size="2px" color="grey-2"/>
+
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PageHome",
+  data() {
+    return {
+      newCweetContent: "",
+    };
+  },
+});
+</script>
+
+<style lang="sass">
+.new-Cweet
+  textarea
+    font-size: 19px
+    line-height: 1.4 !important
+</style>

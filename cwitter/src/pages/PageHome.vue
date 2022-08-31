59fdb906f6d32777033121ce05e329bb519a43b8
Rexarrascal <serkanozytu@gmail.com>
2022-08-31 23:03:09 +0300

    Add functions(create a tweet and delete a tweet)

--- cwitter/src/pages/PageHome.vue
+++ cwitter/src/pages/PageHome.vue
@@ -18,34 +18,130 @@
           </template>
         </q-input>
       </div>
-      
+
       <div class="col col-shrink">
-          <q-btn 
-            :disable="!newCweetContent"
-            class="q-mb-lg"
-            uneleavated
-            rounded
-            color="primary" 
-            label="Cweet"
-            no-caps
-          />
+        <q-btn
+          @click="addNewCweet"
+          :disable="!newCweetContent"
+          class="q-mb-lg"
+          uneleavated
+          rounded
+          color="primary"
+          label="Cweet"
+          no-caps
+        />
       </div>
     </div>
 
-    <q-separator size="2px" color="grey-2"/>
+    <q-separator size="2px" color="grey-2" />
 
+    <q-list separator>
+      <transition-group
+        appear
+        enter-active-class="animated fadeIn"
+        leave-active-class="animated fadeOut"
+      >
+        <q-item 
+          v-for="cweet in cweets" 
+          :key="cweet.date" 
+          class="q-py-md"
+        >
+          <q-item-section avatar>
+            <q-avatar>
+              <img src="https://cdn.quasar.dev/img/avatar5.jpg" />
+            </q-avatar>
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label>
+              <strong>Serkan Oz</strong>
+              <span class="text-grey-7"> @oz_serkan</span>
+            </q-item-label>
+            <q-item-label class="cweet-content text-body1">
+              {{ cweet.content }}
+            </q-item-label>
+            <div class="cweet-icons row justify-between">
+              <q-btn 
+                flat 
+                round 
+                color="grey" 
+                icon="far fa-comment" 
+                size="sm" 
+              />
+              <q-btn 
+                flat 
+                round 
+                color="grey" 
+                icon="fas fa-retweet" 
+                size="sm" 
+              />
+              <q-btn 
+                flat 
+                round 
+                color="grey" 
+                icon="far fa-heart" si
+                ze="sm" 
+              />
+              <q-btn 
+                @click="deleteCweet" 
+                flat round 
+                color="grey" 
+                icon="fas fa-trash" 
+                size="sm" 
+              />
+            </div>
+          </q-item-section>
+
+          <q-item-section side top>
+            {{ cweet.date | relativeDate }}
+          </q-item-section>
+        </q-item>
+      </transition-group>
+    </q-list>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from "vue";
+import { formatDistance } from "date-fns";
+import { moment } from 'moment'
 
 export default defineComponent({
   name: "PageHome",
   data() {
     return {
       newCweetContent: "",
+      cweets: [
+        {
+          content: "Random content",
+          date: 1661973142918,
+        },
+        {
+          content: "Random content 2",
+          date: 1661972581011,
+        },
+      ],
     };
+  },
+  methods: {
+    addNewCweet() {
+      let newCweet = {
+        content: this.newCweetContent,
+        date: Date.now(),
+      };
+      this.cweets.unshift(newCweet)
+      this.newCweetContent = ''
+    },
+    deleteCweet(cweet) {
+      let dateToDelete = cweet.date
+      let index = this.cweets.findIndex(cweet => cweet.date === dateToDelete)
+      this.cweets.splice(index, 1)
+    }
+  },
+  filters: {
+    relativeDate(value) {
+      return formatDistance(value, new Date(), { addSuffix: true });
+    },
   },
 });
 </script>
@@ -55,4 +151,6 @@ export default defineComponent({
   textarea
     font-size: 19px
     line-height: 1.4 !important
+.cweet-icons
+  margin-left: -5px
 </style>
